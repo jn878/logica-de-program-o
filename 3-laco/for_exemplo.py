c70a027a4aa128f2bfdfc61ad1739af3ada2bdff
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-18 16:46:52 -0300

    adicionando laço de rep

---
+++ 3-laco/for_exemplo.py
@@ -0,0 +1,7 @@
+import os
+os.system("cls")
+
+for i in range(1,6):
+    print(f"0 numero e: {i}")
+
+print("fim do programa.")
---
+++ 3-laco/tabuada.py
@@ -0,0 +1,18 @@
+import os
+os.system("cls")
+
+numero = int(input("digite um numero"))
+
+
+for i in range(1,11):
+    print(f"{numero} * {i} = {numero * i}")
+
+for i in range(1,11):
+    print(f"{numero} + {i} = {numero + i}")
+
+for i in range(1,11):
+    print(f"{numero} - {i} = {numero - i}")
+
+for i in range(1,11):
+    print(f"{numero} / {i} = {numero / i}")
+

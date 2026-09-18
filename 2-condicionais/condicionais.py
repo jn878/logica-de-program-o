b987ccaecd5d77bf4baabf7996598445c15f6ed5
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-18 14:51:52 -0300

    Organizando repositorio

--- 2-condicionais/condicionais.py
+++ 2-condicionais/condicionais.py
@@ -1,4 +1,4 @@
-import os 
+import os
 os.system("cls")
 
 numero = int(input("digite um numero"))
--- produto.py
+++
@@ -1,8 +0,0 @@
-descricao = input("nome do produto")
-quantidade =  int(input("quantidade adquirida"))
-preco = float(input("preco unitario"))
-
-total = quantidade * preco
-desconto = desconto - total
-
-if quantidade <= 5 
